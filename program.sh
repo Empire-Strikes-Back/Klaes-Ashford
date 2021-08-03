99bb1cca6a6d453526ddf31548796ab2a158fb91
rocinante <rocinante@program>
2021-08-03 15:38:50 +0300

    to program.sh i add j-package

--- program.sh
+++ program.sh
@@ -26,5 +26,30 @@ uberjar(){
   mv out/rocinante.standalone.jar out/jpackage-input/
 }
 
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting compilation..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut"
+  else
+      J_ARG=""
+  fi
+
+  jpackage \
+    --input out/jpackage-input \
+    --dest out \
+    --main-jar rocinante.standalone.jar \
+    --name "rocinante" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments rocinante.main \
+    --app-version "1" \
+    $J_ARG
+}
 
 "$@"
